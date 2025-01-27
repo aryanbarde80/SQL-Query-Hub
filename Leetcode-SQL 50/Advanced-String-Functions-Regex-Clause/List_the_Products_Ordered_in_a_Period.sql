10f904980f7be7a5532483d93b36bd2444f8ce5b
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2025-01-27 10:55:53 +0530

    All problem sets solved 😄

---
+++ Leetcode-SQL 50/Advanced-String-Functions-Regex-Clause/List_the_Products_Ordered_in_a_Period.sql
@@ -0,0 +1,7 @@
+SELECT product_name, SUM(unit) AS unit
+FROM Orders AS o
+INNER JOIN Products
+USING(product_id)
+WHERE TO_CHAR(order_date, 'YYYY-MM') = '2020-02'
+GROUP BY product_name
+HAVING SUM(unit) >= 100;
